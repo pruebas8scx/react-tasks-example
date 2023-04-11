03cbfc2c058a87ad730a2857737e1ab8cb270d62
Kevin <jdkkai64@gmail.com>
2023-04-11 00:18:16 -0600

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://pruebas8scx.github.io/react-tasks-example'
 })
